e3de71d33daad4e5568c9f40a5ac8e8679bfde11
Cristian Javier Alvarez <cjavi26000@hotmail.com>
2019-10-07 13:12:41 -0600

    Creacion de un archivo de localstorage

--- routes/smash.js
+++ routes/smash.js
@@ -1,18 +1,20 @@
 const express = require('express');
 const router = express.Router();
+var storage = require('../archivos/localstorage.json');
 var arreglo = [];
 var encontrado;
-character = {
+/*character = {
     "personajes": [
         {"id":"1", "nombre":"Mario", "franquicia":"Super Mario", "descripcion":"Heroe del nintendo"},
         {"id":"2", "nombre":"Kirby", "franquicia":"Kirby Super Star", "descripcion":"Consentido de Sakurai"},
         {"id":"3", "nombre":"Fox", "franquicia":"Star Fox", "descripcion":"Lider de Star Fox"}
     ]
-}
+}*/
+character = storage;
 
 
 router.get('/', (req, res, next) => {
-    console.log(character.personajes.length);
+    console.log(character);
     res.status(200).json(character);
 });
 
