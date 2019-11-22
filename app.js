853028d09ccc1ac8734f24c99130de0888d22d8a
Cristian Javier Alvarez <cjavi26000@hotmail.com>
2019-11-22 14:22:01 -0600

    Correción de Cors

--- app.js
+++ app.js
@@ -11,12 +11,15 @@ const {mongoose} = require('./database');
 
 app.set('port', process.env.PORT || 3000);
 app.use(express.json());
-app.use(cors({origin: '*'}));
+//app.use(cors({origin: '*'}));
 
-/*app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', '*');
-  next();
-});*/
+app.use((req, res, next) => {
+	res.header('Access-Control-Allow-Origin', '*');
+	res.header('Access-Control-Allow-Headers', 'Authorization, X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Allow-Request-Method');
+	res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
+	res.header('Allow', 'GET, POST, OPTIONS, PUT, DELETE');
+	next();
+});
 
 //Routes
 app.use('/api/v1/smash', smashRouter);
